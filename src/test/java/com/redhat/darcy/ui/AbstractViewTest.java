edb135a6f22fcfd7471bf1c7e01040c53d9b6f59
Alec Henninger <alechenninger@gmail.com>
2014-03-30 13:12:57 -0400

    Update package name (missed one)

--- src/test/java/com/redhat/darcy/ui/AbstractViewTest.java
+++ src/test/java/com/redhat/darcy/ui/AbstractViewTest.java
@@ -17,7 +17,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.redhat.uiautotool.ui;
+package com.redhat.darcy.ui;
 
 public class AbstractViewTest {
     
