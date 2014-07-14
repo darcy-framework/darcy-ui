1b0438d4b6163bbf117867bdbc17b509145f907b
Alec Henninger <alechenninger@gmail.com>
2014-07-14 19:20:21 -0400

    Add to ChainedElementContextTest

--- src/test/java/com/redhat/darcy/ui/ChainedElementContextTest.java
+++ src/test/java/com/redhat/darcy/ui/ChainedElementContextTest.java
@@ -1,4 +1,24 @@
 package com.redhat.darcy.ui;
 
+import static com.redhat.darcy.ui.ChainedElementContext.makeChainedElementContext;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+import com.redhat.darcy.ui.testing.doubles.DummyContext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class ChainedElementContextTest {
+    @Test
+    public void shouldImplementAllInterfacesOfParentContext() {
+        ElementContext context = makeChainedElementContext(new TestContext(), null);
+
+        assertThat(context, instanceOf(TestInterface.class));
+    }
+
+    interface TestInterface {}
+    class TestContext extends DummyContext implements TestInterface {}
 }
