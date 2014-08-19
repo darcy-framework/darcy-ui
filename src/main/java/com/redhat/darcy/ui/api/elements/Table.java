ec156756905bf9e222d6a058ea473a18da807a22
Alec Henninger <alechenninger@gmail.com>
2014-08-19 15:27:04 -0400

    Experiment with table interfaces and classes

--- src/main/java/com/redhat/darcy/ui/api/elements/Table.java
+++ src/main/java/com/redhat/darcy/ui/api/elements/Table.java
@@ -19,27 +19,102 @@
 
 package com.redhat.darcy.ui.api.elements;
 
-import com.redhat.darcy.ui.api.Context;
-import com.redhat.darcy.ui.api.ElementSelection;
-import com.redhat.darcy.ui.api.Selection;
+import com.redhat.darcy.ui.api.ViewElement;
 
 import org.hamcrest.Matcher;
 
-public interface Table<T> {
-    TableRow getRow(int row);
-    <U extends T, E> TableColumn<E> getColumn(U column);
-    <E> E getCell(T column, int row);
-    <E> E getCell(T column, Matcher<? super E> matcher);
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-    class ColumnA implements TableColumn<String> {
+public interface Table<T extends Table<T>> extends ViewElement {
+    @SuppressWarnings("unchecked")
+    default Row<T> getRow(int row) {
+        return new Row<>((T) this, row);
+    }
 
-        @Override
-        public String getCell(int row) {
-            return find().text(byInner(By.xpath))
+    @SuppressWarnings("unchecked")
+    default <U> Column<ColumnDefinition<T, U>, T, U> getColumn(ColumnDefinition<T, U> column) {
+        return new Column<>((T) this, column);
+    }
+
+    @SuppressWarnings("unchecked")
+    default <U> U getCell(ColumnDefinition<T, U> column, int row) {
+        return column.getCell((T) this, row);
+    }
+
+    Iterable<Row<T>> rows();
+
+    default <U> Stream<Row<T>> getRowsWhere(ColumnDefinition<T, U> column, Predicate<? super U> predicate) {
+        return StreamSupport.stream(rows().spliterator(), false)
+                .filter(r -> predicate.test(r.getCell(column)));
+    }
+
+    default <U> Stream<Row<T>> getRowsWhere(ColumnDefinition<T, U> column, Matcher<? super U> matcher) {
+        return getRowsWhere(column, matcher::matches);
+    }
+
+    /*
+    boolean isEmpty();
+    int getRowCount();
+
+    // interface PaginatedTable
+    T toPage(int page);
+    T previousPage();
+    T nextPage();
+    boolean hasNextPage();
+    boolean hasPreviousPage();
+    int getCurrentPage();
+    int getTotalEntries();
+    Iterable<...?> ascendingPages();
+
+    // interface LazyPaginatedTable?
+    Optional<Integer> getMaxPages(); // Actually, let's consider leaving this to a roll interface
+                                     // so a table can have this as non optional if its there and
+                                     // just leave it off the api if not
+
+    // interface SortableTable
+    void sort(column, SortDirection direction);
+    SortDirection getSortDirectionOfHeader(column);
+     */
+
+    final class Column<T extends ColumnDefinition<U, E>, U extends Table<U>, E> {
+        private final U table;
+        private final T column;
+
+        public Column(U table, T column) {
+            this.table = table;
+            this.column = column;
+        }
+
+        public E getCell(int row) {
+            return column.getCell(table, row);
         }
     }
 
-    class Columns {
-        static TableColumn<String> COLUMN_A = new ColumnA();
+    final class Row<T extends Table<T>> {
+        private final T table;
+        private final int index;
+
+        public Row(T table, int index) {
+            this.table = table;
+            this.index = index;
+        }
+
+        public <U> U getCell(ColumnDefinition<T, U> column) {
+            return column.getCell(getTable(), getIndex());
+        }
+
+        public T getTable() {
+            return table;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    interface ColumnDefinition<T extends Table<T>, U> {
+        U getCell(T table, int row);
     }
 }
