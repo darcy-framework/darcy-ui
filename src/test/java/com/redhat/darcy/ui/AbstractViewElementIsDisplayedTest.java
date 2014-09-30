95e8bca45e752215f8530aa4ec8bcf50bcd70576
Alec Henninger <alechenninger@gmail.com>
2014-09-30 16:27:00 -0400

    Unused import

--- src/test/java/com/redhat/darcy/ui/AbstractViewElementIsDisplayedTest.java
+++ src/test/java/com/redhat/darcy/ui/AbstractViewElementIsDisplayedTest.java
@@ -39,8 +39,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.List;
-
 @RunWith(JUnit4.class)
 public class AbstractViewElementIsDisplayedTest {
     @Test(expected = NoRequiredElementsException.class)
