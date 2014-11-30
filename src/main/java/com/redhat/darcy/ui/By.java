9f930eb911dd3d4e48aac61b17ee9f2af18f88a2
Alec Henninger <alechenninger@gmail.com>
2014-11-30 00:09:15 -0500

    Update copyrights

--- src/main/java/com/redhat/darcy/ui/By.java
+++ src/main/java/com/redhat/darcy/ui/By.java
@@ -1,7 +1,7 @@
 /*
  Copyright 2014 Red Hat, Inc. and/or its affiliates.
 
- This file is part of darcy.
+ This file is part of darcy-ui.
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
